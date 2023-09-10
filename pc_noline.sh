dbed616474b21d4e03ad15f26d3ba201e7bd4efc
youpingfang <18656098638@163.com>
2023-09-10 20:25:14 +0800

    测试代码

---
+++ pc_noline.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Git Commit and Push Script"
+
+# 将更改添加到暂存区
+git add .
+
+# 提示用户输入提交消息
+echo "Enter your commit message:"
+read commit_message
+
+# 执行提交操作
+git commit -m "$commit_message"
+
+# 推送更改到GitHub
+git push origin master
+
+echo "Git Commit and Push Complete"
