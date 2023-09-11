88d93bee7dc3add1577f1db7df474672144f2aaf
youpingfang <18656098638@163.com>
2023-09-11 22:23:25 +0800

    test

--- pc_noline.sh
+++ pc_noline.sh
@@ -2,9 +2,12 @@
 
 echo "Git Commit and Push Script"
 
-# 将更改添加到暂存区
-git pull
+# 进入指定目录
+cd "C:\Users\zad\Desktop\bat_tool"
 
+# 执行 git pull 操作以同步远程仓库
+git pull 
+# 将更改添加到暂存区
 git add .
 
 # 提示用户输入提交消息
